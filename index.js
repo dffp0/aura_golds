bd5467a81c135f08b390ace3739e2735219778e8
dffp0 <ar321ar@gmail.com>
2026-02-03 02:01:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("السيرفر شغال 🎉");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
