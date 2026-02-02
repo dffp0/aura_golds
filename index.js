ac8ae885f81a80ae8809dd435d654cd35c5c8be5
dffp0 <ar321ar@gmail.com>
2026-02-03 02:06:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors()); // يسمح لموقعك على Netlify يكلم السيرفر
 
 app.get("/", (req, res) => {
   res.send("السيرفر شغال 🎉");
 });
 
-const PORT = process.env.PORT || 3000;
+// API للاختبار
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok", time: new Date().toISOString() });
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
